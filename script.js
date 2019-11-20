068ea3733faa38cdc037573c8cf524805f77cc34
Hansel <hchavezblanco@hotmail.com>
2019-11-19 23:21:12 -0700

    Added all files

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", () => {
+    const background = document.querySelector(".main .background");
+    const titleh1 = document.querySelector(".main h1");
+    var scroll = window.pageYOffset;
+    var titleMove = scroll * .7;
+    var backgroundMove = scroll * .8;
+
+    titleh1.style.transform = `translate3d(0, ${titleMove}px, 0)`;
+    titleh1.style.opacity = `${100 - titleMove * .3}%`;
+
+
+    background.style.transform = `translate3d(0, ${backgroundMove}px, 0)`;
+  });
