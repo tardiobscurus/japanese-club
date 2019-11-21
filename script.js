0d3a2d915fb60712e1ee6caac2a3e3cd982db256
Hansel <hchavezblanco@hotmail.com>
2019-11-20 21:58:32 -0700

    Have added more detail to many files

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ window.addEventListener("scroll", () => {
     const titleh1 = document.querySelector(".main h1");
     const arrow = document.querySelector(".main h2");
     var scroll = window.pageYOffset;
-    var titleMove = scroll * .7;
-    var backgroundMove = scroll * .8;
+    var titleMove = scroll * .55;
+    var backgroundMove = scroll * .7;
 
     titleh1.style.transform = `translate3d(0, ${titleMove}px, 0)`;
-    titleh1.style.opacity = `${100 - titleMove * .3}%`;
+    titleh1.style.opacity = `${100 - titleMove * .4}%`;
 
     console.log(scroll);
 
